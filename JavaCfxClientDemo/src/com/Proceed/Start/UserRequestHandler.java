c973770cfa3b3340b53a0ace8ee2eab7c79bdf50
sunilm <sunil.mohanty@mindfiresolutions.com>
2014-03-17 11:38:05 +0530

    initial commit by Sunil.Mohanty on the behalf of practice

---
+++ JavaCfxClientDemo/src/com/Proceed/Start/UserRequestHandler.java
@@ -0,0 +1,15 @@
+package com.Proceed.Start;
+/**
+ * This belong to crazy experiment , if any body got access to this one by mistake  ,please ignore
+ *
+ * @author sunilm
+ *
+ */
+public class UserRequestHandler {
+
+	public UserRequestHandler()
+	{
+		System.out.println("testing user handler request.java");
+	}
+	
+}
